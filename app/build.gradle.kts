d63d946ac88516e79fc3c15d215c5ac7fafe4998
milenacr25 <milenacarolini25@gmail.com>
2023-11-23 19:32:14 -0300

    Implementado os seletores de tela

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -4,12 +4,12 @@ plugins {
 
 android {
     namespace = "br.com.alfaumuarama.rotafacil"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "br.com.alfaumuarama.rotafacil"
         minSdk = 24
-        targetSdk = 33
+        targetSdk = 34
         versionCode = 1
         versionName = "1.0"
 
---
+++ app/src/main/java/br/com/alfaumuarama/rotafacil/LoginActivity.java
@@ -0,0 +1,30 @@
+package br.com.alfaumuarama.rotafacil;
+
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class LoginActivity extends AppCompatActivity {
+
+
+    EditText edtEmail, edtSenha;
+    Button btnLogin;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        edtEmail = findViewById(R.id.edtEmail);
+        edtSenha = findViewById(R.id.edtSenha);
+        btnLogin = findViewById(R.id.btnLogin);
+
+
+    }
+}
