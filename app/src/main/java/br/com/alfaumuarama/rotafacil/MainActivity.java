3f6b3835cf532a9fa7e84314404a926cefce611b
João Rei Delas <joaogabriel7303@gmail.com>
2023-11-23 23:26:34 -0300

    Criado concção e listagem de todos os usuarios cadastrados

--- app/src/main/java/br/com/alfaumuarama/rotafacil/MainActivity.java
+++ app/src/main/java/br/com/alfaumuarama/rotafacil/MainActivity.java
@@ -1,14 +1,41 @@
 package br.com.alfaumuarama.rotafacil;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.nfc.Tag;
 import android.os.Bundle;
+import android.util.Log;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
 
 public class MainActivity extends AppCompatActivity {
+
+    FirebaseFirestore db = FirebaseFirestore.getInstance();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        db.collection("usuario")
+                .get()
+                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                        if(task.isSuccessful()){
+                            for (QueryDocumentSnapshot document : task.getResult()) {
+                                Log.d("Usuarios", document.getId() + " => " + document.getData());;
+                            }
+                        }else {
+                            Log.w("Eror","Error getting documents.", task.getException());
+                        }
+                    }
+                });
+
     }
 }
